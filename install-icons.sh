7382c4dac7c9f23ce53f8042574375b189cf749d
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-04-13 03:21:00 -0500

    Use tar.gz instead of zip to archive icons

--- install-icons.sh
+++ install-icons.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-# Download the archive with icons.
-curl -fLO https://drive.google.com/uc?export=download&id=0B8DNb95GQj5KZHdXWmVyUDlwd1E
+# Download and unzip the archive with icons.
+curl -fL "https://drive.google.com/uc?export=download&id=0B8DNb95GQj5KZjB0TV9TNF9zV00" | tar -xz
 
-# Unzip and delete the archive.
-unzip icons.zip && rm icons.zip
-
-## Install the icon sets
+# Install the icon sets
 cd icons
-for f in *.iconjar; do
-  open -a Iconjar $f;
-done
+for f in *.iconjar; do open -a Iconjar $f; done
 
 echo "All done! You can use these awesome icons now. :)"
